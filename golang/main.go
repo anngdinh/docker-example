e34d96b7a889a6aaae8122eac6689e7be976a7fa
andrew <an.ngdinh@gmail.com>
2023-11-15 16:10:46 +0700

    :golang:

--- golang/main.go
+++ golang/main.go
@@ -10,8 +10,16 @@ import (
 )
 
 func main() {
+	i := 0
 	for {
-		testDB()
+		i++
+		j := i % 2
+		switch j {
+		case 0:
+			testDB()
+		case 1:
+			testDB2()
+		}
 		fmt.Println("Test DB oke !")
 		time.Sleep(10 * time.Second)
 	}
@@ -29,3 +37,28 @@ func testDB() {
 	}
 	defer insert.Close()
 }
+
+func testDB2() {
+	db, err := sql.Open("mysql", os.Getenv("URL"))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT * FROM sbtest1 WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = stmt.QueryRow("localhost").Err()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmt.Close()
+}
+
+/*
+export QUERY='SELECT * FROM sbtest1'
+export URL='annd2:password@tcp(localhost:3306)/hihi'
+
+*/
